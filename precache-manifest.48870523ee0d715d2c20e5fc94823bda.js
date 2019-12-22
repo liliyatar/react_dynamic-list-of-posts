2bbdcbd8de3d0113d30a0e157cc411d8f071bad3
Liliya Taran <liliyataran2017@gmail.com>
2019-12-22 15:02:23 +0200

    Updates

---
+++ precache-manifest.48870523ee0d715d2c20e5fc94823bda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3576b25d6b0c9e375c8d1ffbf71445f",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "02f58e481fccd830f10e",
+    "url": "/react_dynamic-list-of-posts/static/css/main.0c4efc11.chunk.css"
+  },
+  {
+    "revision": "0a660b30eeb6f888d7e2",
+    "url": "/react_dynamic-list-of-posts/static/js/2.12db86b6.chunk.js"
+  },
+  {
+    "revision": "02f58e481fccd830f10e",
+    "url": "/react_dynamic-list-of-posts/static/js/main.ca83f8cd.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
